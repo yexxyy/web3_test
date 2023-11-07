a3596a715ea9f27ff6b0f804a6017caee5da69b7
yetongxue <yexyog@qq.com>
2023-11-07 14:50:59 +0800

    devloy v2

--- deploy/004_deploy_test_upgrade_v2.ts
+++ deploy/004_deploy_test_upgrade_v2.ts
@@ -3,26 +3,18 @@ import { DeployFunction } from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployer } = await hre.getNamedAccounts();
-    const { deploy } = hre.deployments;
+    const { deploy, execute } = hre.deployments;
 
     // proxy only in non-live network (localhost and hardhat network) enabling HCR (Hot Contract Replacement)
     // in live network, proxy is disabled and constructor is invoked
-    await deploy('UpgradeContract', {
+    const upgrade_contract = await deploy('UpgradeContractV2', {
+        contract: 'UpgradeContract',
         log: true,
-        from: deployer,
-        proxy: {
-            proxyContract: "TestProxy",
-            proxyArgs: ["{implementation}", "{data}"],
-            execute: {
-                init: {
-                    methodName: "initialize",
-                    args: [],
-                }
-            }
-        }
+        from: deployer
     });
+    await execute("UpgradeContract", { from: deployer, log: true }, "upgradeTo", upgrade_contract.address)
     return true
 };
 export default func;
-func.id = 'deploy_test_upgrade'; // id required to prevent reexecution
-func.tags = ['all'];
+func.id = 'deploy_test_upgrade_v2'; // id required to prevent reexecution
+func.tags = ['all_v2'];
